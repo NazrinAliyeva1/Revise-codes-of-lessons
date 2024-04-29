662a65bc1bf8b043213f2c68b0c0e6e1f729d350
NazrinAli1 <anazrin711@gmail.com>
2024-04-28 23:22:23 -0600

    revise codes

---
+++ assets/js/main.js
@@ -0,0 +1,72 @@
+// fetch("https://hp-api.onrender.com/api/characters")
+// .then(response=>response.json())
+
+
+
+
+
+//   .then(data=>{
+//     getCharacters(data)
+//   })    
+// console.log(document.getElementById("cards-area").innerHTML);
+
+// var area = document.getElementById("cards-area");
+
+// function getCharacters(characters) {
+//     area.innerHTML = "";
+//     characters.forEach(char => {
+//         area.innerHTML += `<div class="col-md-3">
+// <div class="card">  
+//     <img src="${char.image}" height="410px" class="card-img-top"
+//         alt="${char.name}"/>
+//     <div class="card-body">
+//         <h5 class="card-title">${char.name}</h5>
+//         <p class="card-text">${char.house} <br> <small>${char.dateOfBirth}</small></p>
+//         <a href="#!" class="btn btn-primary" data-mdb-ripple-init>About</a>
+//     </div>
+// </div>
+// </div>`;
+//     })
+// } 
+
+
+var list = document.getElementById("list-area")
+var nextBtn = document.getElementById("btn-nxt");
+var prevBtn = document.getElementById("btn-prv");
+
+fetchData("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0");
+
+function getPokemons(data) {
+    list.innerHTML = "";
+    nextBtn.setAttribute("page-link", data.next ?? "#");
+    prevBtn.setAttribute("page-link", data.previous ?? "#");
+    data.results.forEach(elem => {
+        list.innerHTML += `<span onclick=getImage("${elem.url}") class="list-group-item list-group-item-action px-3 border-0 bg-qara">${elem.name}</span>`
+    });
+}
+
+function fetchData(link) {
+    fetch(link)
+        .then(res => res.json())
+        .then(data => getPokemons(data));
+}
+
+
+function getImage(link) {
+    fetch(link)
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById("img-area").innerHTML = `<img class="w-100" src="${data.sprites.other.dream_world.front_default}" alt="">`
+        });
+ }
+
+
+nextBtn.onclick = function (e) {
+    fetchData(e.target.getAttribute("page-link"));
+}
+
+
+prevBtn.onclick = function (e) {
+    fetchData(e.target.getAttribute("page-link"));
+}
+
